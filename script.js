27e08bd05fc88634620f727d9fa0c4b0c914bd77
ys_hy <149667956+yslee11@users.noreply.github.com>
2025-12-29 15:23:48 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 /***** ✅ 사용자가 직접 수정해야 하는 부분 *****/
 // 깃허브 저장소 정보 입력
 const GITHUB = {
-  owner: "littledoor-ai",      // ✅ 본인 깃허브 ID
-  repo: "survey-project",       // ✅ 저장소 이름
+  owner: "yslee11",      // ✅ 본인 깃허브 ID
+  repo: "test",       // ✅ 저장소 이름
   branch: "main",               // ✅ 브랜치 (보통 main)
   path: "images"                // ✅ 이미지 폴더 이름
 };
 
 // Google Apps Script Web App URL 입력
 // ✅ Apps Script 코드를 수정한 후 새 배포 URL을 여기에 붙여넣으세요.
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwZneOnZe0ZgqQAZ1Ix19NbQbwHybU4WNbiAd02DGIcThiBaXb4rTRPvZwqcGr7b2RS/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyIJwHYxoSBCJEL54o2uT6HapkVOqcU6jQjES1k6Jr441U109V5TuNqYm3YcvY-4svj/exec";
 
 /*****************************************************/
 
